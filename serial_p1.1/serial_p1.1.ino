0a4142c10739704c8b41c4fe3de295c8fd4a44f9
Germangomezmartin <gomezmartingerman@yahoo.es>
2015-02-06 20:50:24 +0100

    lokoloko

---
+++ serial_p1.1/serial_p1.1.ino
@@ -0,0 +1,10 @@
+void setup()      // run once, when the sketch starts
+{
+  Serial.begin(9600);   // set up Serial library at 9600 bps
+  Serial.println("Hello word!"); // prints with brake line
+}
+
+void loop()   // runover and over again
+{
+              // do nothing
+}             
