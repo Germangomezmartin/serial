c9f6621585d24eee108f37a3e3f7cd2114b592d1
Germangomezmartin <gomezmartingerman@yahoo.es>
2015-03-02 20:49:12 +0100

    02/02/15

---
+++ serial_p4.3b/serial_p4.3b.ino
@@ -0,0 +1,26 @@
+int tempAigua = 99;
+
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  if ( tempAigua < 90)
+  {
+    Serial.print("Aigua encara no bull!");
+  } 
+ 
+ else if ( tempAigua >= 90 && tempAigua < 100)
+  {
+    Serial.print("Aigua apunt de bullir!");
+  } 
+
+else 
+  {
+    Serial.print("Aigua bullint!");
+  }
+}
+void loop()   // we need this to be here even though its empty
+{
+}
+
+
